84330f88efb925003c23dce83432a6b60f38cdfe
BSK <brad.bsk@gmail.com>
2026-03-10 21:41:47 +1100

    Delete BSK_SafeRecordStop.lua

--- BSK_SafeRecordStop.lua
+++
@@ -1,5 +0,0 @@
--- Stop recording only if currently recording
-local playState = reaper.GetPlayState()
-if playState == 4 or playState == 5 then
-    reaper.Main_OnCommand(1016, 0) -- Transport: Stop
-end
